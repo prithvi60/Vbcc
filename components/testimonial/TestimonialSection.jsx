a7d1a78ee04797c675426f4b3e312275f39d5caf
Prithvi <prithvin19@gmail.com>
2024-02-16 22:29:57 +0530

    carousal

--- components/testimonial/TestimonialSection.jsx
+++ components/testimonial/TestimonialSection.jsx
@@ -1,15 +1,19 @@
 "use client";
 import othersData from "@/libs/others.json";
 import Image from "next/image";
-import {  useState } from "react";
+import { useState } from "react";
 import { m, AnimatePresence, LazyMotion } from "framer-motion";
+import ReactSimplyCarousel from "react-simply-carousel";
+
 import dynamic from "next/dynamic";
+import { customHorizontalAnimation } from "@/libs/slider_animation";
 // Make sure to return the specific export containing the feature bundle.
 const loadFeatures = () =>
   import("../../libs/framer_feature").then((res) => res.default);
 const FsLightbox = dynamic(() => import("fslightbox-react"), { ssr: false });
 export const TestimonialSection = () => {
   const [selectedItem, SetselectedItem] = useState(0);
+  const [activeSlideIndex, setActiveSlideIndex] = useState(0);
   const fsTool = othersData.testimonials.map((val) => val.pdf);
   const [lightboxController, setLightboxController] = useState({
     toggler: false,
@@ -68,7 +72,7 @@ export const TestimonialSection = () => {
           Testimonials
         </h2>
         <div className="w-full lg:w-96 2xl:w-1/2 lg:h-auto overflow-hidden flex gap-4">
-          <div className="flex flex-nowrap lg:flex-wrap items-center gap-3 h-[70px] lg:h-full animate-horizontal_carousel lg:animate-none pause mt-2">
+          <div className="hidden md:flex flex-nowrap lg:flex-wrap items-center gap-3 h-[70px] lg:h-full animate-horizontal_carousel lg:animate-none pause mt-2">
             {othersData.testimonials.map((item, idx) => (
               <div
                 className={`p-1 rounded-md ${
@@ -95,6 +99,49 @@ export const TestimonialSection = () => {
             ))}
           </div>
         </div>
+        <div className="md:hidden">
+          <ReactSimplyCarousel
+            activeSlideIndex={activeSlideIndex}
+            onRequestChange={setActiveSlideIndex}
+            onAfterChange={(o) => {
+              // console.log("change", o, selectedItem);
+              SetselectedItem(o);
+            }}
+            itemsToShow={4}
+            itemsToScroll={1}
+            speed={800}
+            easing={"linear"}
+            infinite
+            autoplay={true}
+            autoplayDelay={1500}
+            autoplayDirection={"forward"}
+          >
+            {othersData.testimonials.map((item, idx) => (
+              <div
+                className={`p-1 rounded-md ${
+                  idx === selectedItem
+                    ? "bg-gradient-to-br from-[#A5005A] to-[#5A00A4] "
+                    : " "
+                } `}
+                key={item.id}
+              >
+                <div
+                  className="relative h-[70px] w-[90px] 2xl:h-[80px] 2xl:w-[100px] bg-white rounded-md cursor-pointer"
+                  onClick={() => handleChangePDF(idx)}
+                >
+                  <Image
+                    fill
+                    src={item.logo}
+                    placeholder="blur"
+                    blurDataURL={`${item.logo}?tr=bl-100`}
+                    alt="Logo"
+                    className="object-contain object-center p-3"
+                  />
+                </div>
+              </div>
+            ))}
+          </ReactSimplyCarousel>
+        </div>
       </div>
       <AnimatePresence initial={true}>
         <div className="lg:w-1/2 w-full h-full z-30 flex items-center justify-start lg:justify-end">
@@ -105,7 +152,7 @@ export const TestimonialSection = () => {
               variants={slideVariants}
               initial="slide"
               animate="visible"
-              transition={{duration: 1.5}}
+              transition={{ duration: 1.5 }}
               exit="exit"
               key={selectedItem}
             >
@@ -122,13 +169,13 @@ export const TestimonialSection = () => {
           </LazyMotion>
         </div>
       </AnimatePresence>
-        <FsLightbox
-          exitFullscreenOnClose={true}
-          toggler={lightboxController.toggler}
-          sources={fsTool}
-          type="image"
-          slide={lightboxController.slide}
-        />
+      <FsLightbox
+        exitFullscreenOnClose={true}
+        toggler={lightboxController.toggler}
+        sources={fsTool}
+        type="image"
+        slide={lightboxController.slide}
+      />
     </section>
   );
 };
