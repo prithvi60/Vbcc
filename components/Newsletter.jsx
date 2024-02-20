90df9f7161bc49cd5137bffe3e4bd5332d063e5c
unknown <gokulgandhi97@gmail.com>
2024-02-20 20:25:31 +0530

    changes

--- components/Newsletter.jsx
+++ components/Newsletter.jsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 const Newsletter = () => {
   return (
-    <section className="px-6 md:px-10 xl:px-16 2xl:px-20 py-16 xl:py-12 2xl:py-16 text-warning">
-      <div className="py-10 px-5 md:p-10 2xl:p-12  space-y-10 2xl:space-y-12 rounded-lg md:rounded-none">
+    <section className="px-6 md:px-10 xl:px-16 2xl:px-20 py-16 xl:py-12 2xl:py-16 text-white">
+      <div className="py-10 px-5 md:p-10 2xl:p-12 bg-primary space-y-10 2xl:space-y-12 rounded-lg md:rounded-none">
         <div className="text-center space-y-9 2xl:space-y-10">
           <h2 className="text-2xl md:text-[32px] xl:text-[40px] 2xl:text-5xl font-Lora">
             Get the Latest Updates
@@ -18,18 +18,18 @@ const Newsletter = () => {
           method="post"
           acceptCharset="UTF-8"
         >
-          <div className="flex flex-wrap sm:flex-nowrap items-center max-w-[320px] md:w-[375px] rounded-full border py-2 px-3 border-black focus:border-secondary mx-auto mb-4 sm:mb-0">
+          <div className="flex flex-wrap sm:flex-nowrap items-center max-w-[320px] md:w-[375px] rounded-full border py-2 px-3 border-secondary focus:border-secondary mx-auto mb-4 sm:mb-0 bg-secondary">
             <input
               required
               name="Email"
               type="email"
               placeholder="Your Email"
-              className="bg-transparent placeholder:text-zinc-500 !border-none !outline-none placeholder:font-Nunito w-[175px] md:w-[198px]"
+              className="bg-secondary placeholder:text-zinc-500 !border-none !outline-none placeholder:font-Nunito w-[175px] md:w-[198px] text-primary text-base md:text-lg"
             />
             {/* px-4 py-2 */}
             <button
               type="submit"
-              className={`hidden sm:block rounded-full px-4 py-2 group bg-primary hover:bg-info text-center text-base duration-700 delay-75 font-urbanist capitalize w-full`}
+              className={`hidden sm:block rounded-full px-4 py-3 group bg-primary hover:bg-info text-center text-base duration-700 delay-75 font-urbanist capitalize w-full`}
             >
               <div className={`h-6 w-full overflow-hidden`}>
                 <h3
@@ -46,22 +46,22 @@ const Newsletter = () => {
             </button>
           </div>
           <button
-              type="submit"
-              className={`block sm:hidden rounded-full px-4 py-2 group bg-primary hover:bg-info text-center text-base duration-700 delay-75 font-urbanist capitalize w-full`}
-            >
-              <div className={`h-6 w-full overflow-hidden`}>
-                <h3
-                  className={`transition translate-y-0 group-hover:-translate-y-20 duration-700 text-white`}
-                >
-                  Subscribe
-                </h3>
-                <h3
-                  className={`translate-y-20 transition group-hover:-translate-y-[25px] duration-700 text-white`}
-                >
-                  Subscribe
-                </h3>
-              </div>
-            </button>
+            type="submit"
+            className={`block sm:hidden rounded-full px-4 py-2 group bg-secondary hover:bg-info text-center text-base duration-700 delay-75 font-urbanist capitalize w-full`}
+          >
+            <div className={`h-6 w-full overflow-hidden`}>
+              <h3
+                className={`transition translate-y-0 group-hover:-translate-y-20 duration-700 text-primary`}
+              >
+                Subscribe
+              </h3>
+              <h3
+                className={`translate-y-20 transition group-hover:-translate-y-[25px] duration-700 text-white`}
+              >
+                Subscribe
+              </h3>
+            </div>
+          </button>
         </form>
       </div>
     </section>
--- components/presence/OurPresenceContact.jsx
+++ components/presence/OurPresenceContact.jsx
@@ -13,14 +13,14 @@ export const OurPresenceContact = ({ title, mobTitle, para }) => {
   };
   return (
     <section className="py-16 px-5 md:py-[60px] md:px-10 xl:px-20 ">
-      <div className="bg-transparent p-10 space-y-10">
-        <h3 className="hidden md:block text-[40px] text-warning text-center font-Lora">
+      <div className="bg-primary p-10 space-y-10">
+        <h3 className="hidden md:block text-[40px] text-white text-center font-Lora">
           {title}
         </h3>
-        <h3 className="block md:hidden text-3xl text-warning text-center font-Lora">
+        <h3 className="block md:hidden text-3xl text-white text-center font-Lora">
           {mobTitle}
         </h3>
-        <p className="text-base md:text-2xl text-warning md:mx-auto font-urbanist w-full md:w-11/12 text-center tracking-tight md:tracking-wide leading-snug opacity-80">
+        <p className="text-base md:text-2xl text-white md:mx-auto font-urbanist w-full md:w-11/12 text-center tracking-tight md:tracking-wide leading-snug opacity-80">
           {para}
         </p>
         <span className="flex flex-col-reverse md:flex-row justify-center items-center gap-3 md:gap-6">
@@ -52,16 +52,16 @@ export const OurPresenceContact = ({ title, mobTitle, para }) => {
               </div>
             </button>
             {Copied && (
-              <div className="absolute -bottom-7 left-0 text-warning text-base text-center w-max">
+              <div className="absolute -bottom-7 left-0 text-white text-base text-center w-max">
                 ✔️Copied to Clipboard
               </div>
             )}
           </div>
           <Btn2
-            Hbgcolor={"bg-primary"}
-            textColor={"text-warning"}
-            HtextColor={"text-white"}
-            bColor={"border-warning group-hover:border-white"}
+            Hbgcolor={"bg-white"}
+            textColor={"text-white"}
+            HtextColor={"text-warning"}
+            bColor={"border-white group-hover:border-white"}
             title={"Contact Us"}
             href={"/contact"}
             background={"transparent"}
