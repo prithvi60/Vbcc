eee562b7b6bb9ddb5e954b82bcfd0d1ecda47cc5
GOGA-23 <gokulgandhi97@gmail.com>
2024-12-12 14:41:12 +0530

    search updated

--- components/ModalForSearch.jsx
+++ components/ModalForSearch.jsx
@@ -1,26 +1,120 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { IoSearch } from "react-icons/io5";
 import { AnimatePresence, motion } from "framer-motion";
+import Fuse from "fuse.js";
+import { POSTS_QUERY } from "@/sanity/Queries";
+import { client } from "@/sanity/lib/client";
+import { DentalProductsList, LabProductsList } from "@/libs/productsData";
+import { useRouter } from "next/navigation";
 
 const ModalForSearch = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const [query, setQuery] = useState("");
+    const [results, setResults] = useState([]);
+    const [blogPosts, setBlogPosts] = useState([]);
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        // Fetch blog posts from Sanity CMS
+        const fetchBlogPosts = async () => {
+            const posts = await client.fetch(POSTS_QUERY);
+            setBlogPosts(posts);
+        };
+
+        fetchBlogPosts();
+    }, []);
+
+    const search = (term) => {
+        setQuery(term);
+
+        // Fuse.js configuration
+        const dentalFuse = new Fuse(DentalProductsList, {
+            keys: ["productName", "desc"],
+            threshold: 0.2, // Adjust for sensitivity
+            includeScore: true,
+            useExtendedSearch: true,
+            findAllMatches: true,
+        });
+
+        const labFuse = new Fuse(LabProductsList, {
+            keys: ["productName", "desc"],
+            threshold: 0.2, // Adjust for sensitivity
+            includeScore: true,
+            useExtendedSearch: true,
+            findAllMatches: true,
+        });
+
+        const blogFuse = new Fuse(blogPosts, {
+            keys: ["title", "blogShortRead"],
+            threshold: 0.2,
+            includeScore: true,
+            useExtendedSearch: true,
+            findAllMatches: true,
+        });
+
+        // Perform search
+        const dentalProductResults = term
+            ? dentalFuse.search(term).map((result) => result.item)
+            : [];
+        const labProductResults = term
+            ? labFuse.search(term).map((result) => result.item)
+            : [];
+        const blogResults = term
+            ? blogFuse.search(term).map((result) => result.item)
+            : [];
+
+        const allResults = [
+            ...dentalProductResults,
+            ...labProductResults,
+            ...blogResults,
+        ];
+        setResults(allResults);
+        setIsVisible(!!allResults.length);
+    };
+    console.log(results);
+
     return (
-        <div className="grid">
+        <div className="grid md:-mt-2">
             <button
                 onClick={() => setIsOpen(true)}
                 className="p-2 bg-white rounded-lg text-primary"
             >
-                <IoSearch className="text-xl md:text-2xl" />
+                <IoSearch className="text-xl md:text-2xl text-info" />
             </button>
-            <SpringModal isOpen={isOpen} setIsOpen={setIsOpen} />
+            <SpringModal
+                isOpen={isOpen}
+                setIsOpen={setIsOpen}
+                query={query}
+                search={search}
+                results={results}
+                isVisible={isVisible}
+            />
         </div>
     );
 };
 
 export default ModalForSearch;
 
-const SpringModal = ({ isOpen, setIsOpen }) => {
+const SpringModal = ({
+    isOpen,
+    setIsOpen,
+    query,
+    search,
+    results,
+    isVisible,
+}) => {
+    const router = useRouter();
+    const handleNavigation = (item) => {
+        const path =
+            item.slug === "dental"
+                ? `/categories/dental/${item.productName.replace(/\s/g, "_")}` // Dental product link
+                : item.slug === "lab"
+                    ? `/categories/laboratory/${item.productName.replace(/\s/g, "_")}` // Lab product link
+                    : `/blog/${item.slug}`; // Blog post link
+        router.push(path); // Navigate to the appropriate route
+        setIsOpen(false);
+    };
     return (
         <AnimatePresence>
             {isOpen && (
@@ -29,30 +123,64 @@ const SpringModal = ({ isOpen, setIsOpen }) => {
                     animate={{ opacity: 1 }}
                     exit={{ opacity: 0 }}
                     onClick={() => setIsOpen(false)}
-                    className="fixed inset-0 !z-[9999] grid w-full h-full p-8 overflow-y-scroll cursor-pointer bg-blue-900/50 backdrop-blur place-items-center font-Montserrat"
+                    className="fixed inset-0 !z-[9999] grid w-full h-full p-8 cursor-pointer bg-blue-900/50 backdrop-blur place-items-center font-Montserrat"
                 >
                     <motion.div
                         initial={{ scale: 0, rotate: "12.5deg" }}
                         animate={{ scale: 1, rotate: "0deg" }}
                         exit={{ scale: 0, rotate: "0deg" }}
                         onClick={(e) => e.stopPropagation()}
-                        className="relative z-[999] w-full max-w-xl text-white rounded-lg shadow-xl cursor-default md:bg-primary md:p-8 md:max-w-3xl"
+                        className="relative z-[999] w-full text-white rounded-lg shadow-xl cursor-default bg-primary p-4 md:p-8 max-w-3xl"
                     >
-                        <div className="flex items-center gap-2">
-                            <div className="block w-full text-primary">
-                                <input
-                                    required
-                                    name="search"
-                                    // value={formData.firstName || ""}
-                                    // onChange={handleChange}
-                                    type="text"
-                                    placeholder="Search Product Name"
-                                    className="w-full px-4 py-3 mt-1 border rounded-md border-primary focus:border-none focus:ring-0 focus:outline focus:outline-success placeholder:text-sm md:placeholder:text-base placeholder:text-blue-700"
-                                />
+                        <div>
+                            <div className="flex items-center gap-2">
+                                <div className="relative block w-full text-primary">
+                                    <input
+                                        required
+                                        name="search"
+                                        value={query}
+                                        onChange={(e) => search(e.target.value)}
+                                        type="text"
+                                        placeholder="Discover the perfect products that meet your needs!"
+                                        className="w-full px-4 py-3 mt-1 border rounded-md border-primary focus:border-none focus:ring-0 focus:outline focus:outline-success placeholder:text-sm md:placeholder:text-base placeholder:text-blue-700 placeholder:opacity-60"
+                                    />
+                                    <div className="absolute right-0 p-2 bg-white top-3 md:top-2">
+                                        <IoSearch className="text-xl md:text-2xl opacity-60 text-info" />
+                                    </div>
+                                </div>
+                                {/* <button className="p-2.5 md:px-8 md:py-3.5 text-base font-semibold tracking-wider bg-info rounded-md text-white">
+                                    Search
+                                </button> */}
                             </div>
-                            <button className="p-2.5 md:px-8 md:py-3.5 text-base font-semibold tracking-wider bg-info rounded-md text-white">
-                                Search
-                            </button>
+                            {(results.length === 0 && query !== "") ? (
+                                <div
+                                    className={`w-full mt-8 transition-all duration-300 ease-in-out searchScroll max-h-10 origin-top`}
+                                >
+                                    <h4 className="text-xl font-medium tracking-wide">{`No results found for ${query}`}</h4>
+                                </div>
+                            ) : (
+                                <div
+                                    className={`w-full mt-3 overflow-y-scroll transition-all duration-300 ease-in-out searchScroll max-h-72  ${isVisible
+                                        ? "opacity-100 max-h-72 scale-y-100"
+                                        : "opacity-0 max-h-0 scale-y-0"
+                                        } origin-top`}
+                                >
+                                    {results.map((item, index) => (
+                                        <button
+                                            key={index}
+                                            onClick={() => handleNavigation(item)}
+                                            className="w-full p-2 text-left  border-b padding-3.5"
+                                        >
+                                            <h3 className="text-lg font-bold underline underline-offset-4">
+                                                {item.productName || item.title}
+                                            </h3>
+                                            <p className="text-sm line-clamp-6 md:line-clamp-none">
+                                                {item.desc || item.blogShortRead}
+                                            </p>
+                                        </button>
+                                    ))}
+                                </div>
+                            )}
                         </div>
                     </motion.div>
                 </motion.div>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -17,154 +17,124 @@ const NavBar = () => {
   return (
     <nav className="absolute left-0 z-10 w-full px-3 py-6 text-white top-1.5 md:-top-2 xl:px-5 md:py-6 xl:py-7 font-Montserrat">
       <div
-        className={`relative flex items-center w-full rounded-2xl justify-between lg:justify-start lg:gap-20`}
+        className={`relative flex items-center w-full gap-4 justify-between`}
       >
-        {/* className={`ml-5 md:ml-0 absolute top-0 md:top-2  ${path === "/categories/dental" || path === "/categories/laboratory"
-              ? "w-36 bg-white rounded-md h-16 md:w-24 md:h-7 -left-8 md:left-2 "
-              : "w-36 h-12 md:w-24 md:h-7 -left-2 md:left-2"
-              } lg:h-9 lg:w-32 2xl:h-16 2xl:w-36 ${(path === "/about" || path === "/categories/dental/" || path === "/categories/laboratory/") && "bg-white !w-36 !-left-9 md:!left-2 !h-16 md:!w-28 md:!h-11 xl:!w-36 xl:!h-14 rounded-md "}`} */}
-        <Link href={"/"}>
-          <div
-            className={`relative  w-32 h-14 md:h-16 md:w-40 2xl:h-20 2xl:w-44 lg:-ml-3`}
-          >
-            {path === "/" || path === "/blog" || path === "/testimonials" || path === "/contact" || path === "/about" || path === "/faq" ? (
-              <Image
-                alt="VBCC Logo"
-                src="https://ik.imagekit.io/webibee/VBCC/homepage/VBCC%20logo.svg?updatedAt=1733742968628"
-                fill
-                className={`object-cover object-center`}
-                priority
-              />
-            ) : (
-              <Image
-                alt="VBCC Logo"
-                src="/VBCC logo.svg"
-                fill
-                className={`object-cover object-center`}
-                priority
-              />
-            )}
-          </div>
-        </Link>
-        <ul className="items-center hidden gap-6 lg:flex">
-          {navLinks.map((list, id) => (
-            <li key={id} className="relative py-4 group">
-              <div
-                className={`relative text-base lg:text-lg font-semibold uppercase tracking-wide decoration-info hover:underline hover:underline-offset-8 z-20 ${list.menu === "CATEGORIES" && "group-hover:text-white"
-                  } group-hover:!z-[1000] ${path === "/" ||
-                    path.startsWith("/blog/") ||
-                    path.startsWith("/categories/") ||
-                    path === "/categories"
-                    ? "text-white"
-                    : "text-primary"
-                  }`}
-              >
-                {list.menu === "CATEGORIES" ? (
-                  <Link href={list.ref} onMouseEnter={() => setIsHover(true)}>
-                    <h4>{list.menu}</h4>
-                  </Link>
-                ) : (
-                  <Link href={list.ref}>
-                    <h4>{list.menu}</h4>
-                  </Link>
-                )}
+        <div className="relative flex items-center justify-between w-full lg:justify-start lg:gap-20">
+          <Link href={"/"}>
+            <div
+              className={`relative  w-32 h-14 md:h-16 md:w-40 2xl:h-20 2xl:w-44 lg:-ml-3`}
+            >
+              {path === "/" || path === "/blog" || path === "/testimonials" || path === "/contact" || path === "/about" || path === "/faq" ? (
+                <Image
+                  alt="VBCC Logo"
+                  src="https://ik.imagekit.io/webibee/VBCC/homepage/VBCC%20logo.svg?updatedAt=1733742968628"
+                  fill
+                  className={`object-cover object-center`}
+                  priority
+                />
+              ) : (
+                <Image
+                  alt="VBCC Logo"
+                  src="/VBCC logo.svg"
+                  fill
+                  className={`object-cover object-center`}
+                  priority
+                />
+              )}
+            </div>
+          </Link>
+          <ul className="items-center hidden gap-6 lg:flex">
+            {navLinks.map((list, id) => (
+              <li key={id} className="relative py-4 group">
+                <div
+                  className={`relative text-base lg:text-lg font-semibold uppercase tracking-wide decoration-info hover:underline hover:underline-offset-8 z-20 ${list.menu === "CATEGORIES" && "group-hover:text-white"
+                    } group-hover:!z-[1000] ${path === "/" ||
+                      path.startsWith("/blog/") ||
+                      path.startsWith("/categories/") ||
+                      path === "/categories"
+                      ? "text-white"
+                      : "text-primary"
+                    }`}
+                >
+                  {list.menu === "CATEGORIES" ? (
+                    <Link href={list.ref} onMouseEnter={() => setIsHover(true)}>
+                      <h4>{list.menu}</h4>
+                    </Link>
+                  ) : (
+                    <Link href={list.ref}>
+                      <h4>{list.menu}</h4>
+                    </Link>
+                  )}
 
-                {/* {list.menu === "BLOGS" && path !== "/" ? (
+                  {/* {list.menu === "BLOGS" && path !== "/" ? (
                     <h4>{`CULTURE OR ${list.menu}`}</h4>
                   ) : (
                     <h4>{list.menu}</h4>
                   )}{" "} */}
-              </div>
-              {list.subMenu && (
-                <div
-                  className={`fixed w-full h-full bg-[#2931719c] bg-opacity-60 top-0 left-0 !z-[999] ${isHover
-                    ? "block transition-all duration-400 ease-linear group"
-                    : "hidden"
-                    }`}
-                >
+                </div>
+                {list.subMenu && (
                   <div
-                    className={`absolute w-full h-[400px] xl:h-[320px] left-48 xl:left-64 md:w-3/4 2xl:w-[55%] top-3 !z-[999]`}
+                    className={`fixed w-full h-full bg-[#2931719c] bg-opacity-60 top-0 left-0 !z-[999] ${isHover
+                      ? "block transition-all duration-400 ease-linear group"
+                      : "hidden"
+                      }`}
                   >
                     <div
-                      onMouseLeave={() => setIsHover(false)}
-                      className="relative w-full h-full px-6 py-8 bg-center bg-no-repeat bg-cover rtl_card-mask"
-                      style={{
-                        backgroundImage: "url('/counterBg.png')",
-                      }}
+                      className={`absolute w-full h-[400px] xl:h-[320px] left-48 xl:left-64 md:w-3/4 2xl:w-[55%] top-3 !z-[999]`}
                     >
-                      <div className="absolute w-full h-full bg-[#293271] bg-opacity-60 top-0 left-0 z-20"></div>
-                      <ul
-                        className={`absolute flex items-center gap-10 xl:gap-5 top-20 xl:top-20 ${path.startsWith("/categories/") || path === "/"
-                          ? "left-32 xl:left-20"
-                          : "lg:left-20 xl:left-80 2xl:left-[380px]"
-                          }`}
+                      <div
+                        onMouseLeave={() => setIsHover(false)}
+                        className="relative w-full h-full px-6 py-8 bg-center bg-no-repeat bg-cover rtl_card-mask"
+                        style={{
+                          backgroundImage: "url('/counterBg.png')",
+                        }}
                       >
-                        {list.subMenu.map((item, idx) => (
-                          <li className="relative" key={idx}>
-                            <Link
-                              href={item.ref}
-                              className="flex flex-row h-full gap-4 xl:flex-col"
-                              onClick={() => setIsHover(false)}
-                            >
-                              <div className="relative overflow-hidden size-12 xl:size-20 !z-[1000] rounded-md">
-                                <Image
-                                  alt=""
-                                  src={item.img}
-                                  fill
-                                  className="object-cover object-center"
-                                />
-                              </div>
-                              <div
-                                className={`text-base font-medium tracking-wide decoration-info capitalize relative !z-[1000] text-white`}
+                        <div className="absolute w-full h-full bg-[#293271] bg-opacity-60 top-0 left-0 z-20"></div>
+                        <ul
+                          className={`absolute flex items-center gap-10 xl:gap-5 top-20 xl:top-20 ${path.startsWith("/categories/") || path === "/"
+                            ? "left-32 xl:left-20"
+                            : "lg:left-20 xl:left-80 2xl:left-[380px]"
+                            }`}
+                        >
+                          {list.subMenu.map((item, idx) => (
+                            <li className="relative" key={idx}>
+                              <Link
+                                href={item.ref}
+                                className="flex flex-row h-full gap-4 xl:flex-col"
+                                onClick={() => setIsHover(false)}
                               >
-                                {item.menu}
-                                <p className="flex items-center gap-2">
-                                  Furnaces
-                                  <span>
-                                    <MdDoubleArrow className="text-2xl text-info" />
-                                  </span>
-                                </p>
-                              </div>
-                            </Link>
-                          </li>
-                        ))}
-                      </ul>
+                                <div className="relative overflow-hidden size-12 xl:size-20 !z-[1000] rounded-md">
+                                  <Image
+                                    alt=""
+                                    src={item.img}
+                                    fill
+                                    className="object-cover object-center"
+                                  />
+                                </div>
+                                <div
+                                  className={`text-base font-medium tracking-wide decoration-info capitalize relative !z-[1000] text-white`}
+                                >
+                                  {item.menu}
+                                  <p className="flex items-center gap-2">
+                                    Furnaces
+                                    <span>
+                                      <MdDoubleArrow className="text-2xl text-info" />
+                                    </span>
+                                  </p>
+                                </div>
+                              </Link>
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
                     </div>
                   </div>
-                </div>
-              )}
-            </li>
-          ))}
-        </ul>
-        {/* <ModalForSearch /> */}
-        {/* {path === "/" ||
-          path.startsWith("/blog/") ||
-          path === "/about" ||
-          path === "/testimonials" ||
-          path === "/blog" ||
-          path === "/categories" ? (
-          <button
-            className={`hidden font-semibold border border-info  w-fit transition-all rounded-full items-center text-sm md:text-base gap-3 capitalize bg-gradient-to-bl from-[#6b778cf8] to-[#ffffff3f] hover:bg-gradient-to-t hover:from-info hover:to-info group`}
-          >
-            <h4 className="px-2 py-2 md:px-6">
-              WE SHIP OUR INSTRUMENTS GLOBALLY
-            </h4>
-            <span className="p-4 border rounded-full border-info group-hover:border group-hover:border-primary group-hover:bg-primary">
-            </span>
-          </button>
-        ) : (
-          <button
-            className={`hidden font-semibold border border-info  w-fit transition-all rounded-full items-center text-sm md:text-base gap-3 capitalize bg-gradient-to-r from-[#102242] to-[#27488180] hover:bg-gradient-to-t hover:from-info hover:to-info group ${path.startsWith("/categories/") ? "hidden" : "lg:flex"
-              }`}
-          >
-            <h4 className="px-2 py-2 md:px-6">
-              WE SHIP OUR INSTRUMENTS GLOBALLY
-            </h4>
-            <span className="p-4 border rounded-full border-info group-hover:border group-hover:border-primary group-hover:bg-primary">
-            </span>
-          </button>
-        )} */}
-
+                )}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <ModalForSearch />
         <div className="block group lg:hidden">
           <div
             onClick={() => setIsOpen(!isOpen)}
