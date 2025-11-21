9d7e30734f0ace5145ddddfa50b90da4278850b3
GOGA-23 <gokulgandhi97@gmail.com>
2025-11-21 15:57:06 +0530

    config file update

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "cdn.webibee.com",
+        hostname: "cdn.shopifyapp.shop",
       },
       {
         protocol: "https",
